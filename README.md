73e5d5c046328d63e5b9a414254c900d0650affb
igorWebMASTER <igor_1917@hotmail.com>
2020-03-14 10:50:43 -0300

    app converted using react hooks

---
+++ README.md
@@ -0,0 +1,2 @@
+#Desafio 07
+An E-commerce React Native app .
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
+
+function Header({ navigation }) {
+  const cartSize = useSelector(state => state.cart.lenght);
+
+  return (
+    <Wrapper>
+      <Container>
+        <Logo />
+        <BasketContainer onPress={() => navigation.navigate('Cart')}>
+          <Icon name="shopping-basket" color="#FFF" size={24} />
+          <ItemCount>{cartSize || 0}</ItemCount>
+        </BasketContainer>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Header;
+
+// export default Header;
